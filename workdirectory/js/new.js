657b05656f4a6512a04e60d38e29dbade5f62b8b
Pavel <akulov_pavel88@mail.ru>
2017-03-21 18:01:48 +0200

    life cycle

--- workdirectory/js/new.js
+++ workdirectory/js/new.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 
+
 export default class NewComponent extends Component{
 
     constructor(props){
-        super(props),
-        function() {
-            console.log('export fine')
-        }
+        super(props);
+
+        console.log("constructor")
     }
 
+    componentWillMount(){
+        console.log('componentWillMountf');
+    }
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
 
     render() {
+        console.log('render');
         return(
-            <div>
-                <h3>New Component</h3>
-                <p>{ this.props.text }</p>
+            <div className= "news">
+                <h3>Some text1</h3>
             </div>
+
         );
 
     }
--- workdirectory/js/script.jsx
+++ workdirectory/js/script.jsx
@@ -5,18 +5,47 @@ import ReactDOM from 'react-dom';
 import NewComponent from './new';
 console.log(React);
 
+var my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+
 class App extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state ={
+            timer: 0,
+            arr :[
+
+            ]
+        };
     }
 
+    componentWillMount(){
+        setInterval(()=>{
+            this.setState({timer: this.state.timer+1});
+        },100);
+    }
 
     render() {
         return(
-            <div style={{border:"1px solid #000", color:"#b68448"}} className ="test">
+            <div style={{border:"1px solid #000", color:"#b68448"}} arry ={this.state.arr}>
                 <h1>App works</h1>
-                <NewComponent text = 'hello from newComp1' />
+                {this.state.timer <50 ? <NewComponent news = {my_news}/> : null}
+                <p>{this.state.timer}</p>
             </div>
         );
     }
