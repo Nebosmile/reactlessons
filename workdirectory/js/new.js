03d62b7f189c6b1304a1465112dca5cb286707a6
Pavel <akulov_pavel88@mail.ru>
2017-03-21 18:46:25 +0200

    collection 2 variants

--- workdirectory/js/new.js
+++ workdirectory/js/new.js
@@ -5,22 +5,35 @@ export default class NewComponent extends Component{
 
     constructor(props){
         super(props);
+        this.rendme =this.props.array.map(function(item,index){
+            return(
+                <div key = {index}>
+                    <div>{item.author}</div>
+                    <div>{item.text}</div>
+                    <div>первый вариант</div>
+                </div>
+
+            )
+        });
     }
     renderItem(item, index){
         return(
             <div key = {index}>
                 <div>{item.author}</div>
                 <div>{item.text}</div>
+                <div>второй вариант</div>
             </div>
 
         )
     }
 
+
+
     render() {
         console.log('this.', this.props.array)
         return(
             <div className= "news">
-                {this.props.array.map(this.renderItem.bind(this))}
+                {this.rendme || this.props.array.map(this.renderItem.bind(this))}
             </div>
 
         );
